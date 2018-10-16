b0c80ebcbe2791a47b5c0a6341fe886307747579
docs007 <34295814+docs007@users.noreply.github.com>
2018-10-16 07:43:51 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,7 +6,7 @@ TARGET_CPU_VARIANT := cortex-a7
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x1000000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/Huawei/Cairo_U00/kernel
+TARGET_PREBUILT_KERNEL := device/huawei/Cairo_U00/kernel
 
 BOARD_MKBOOTIMG_ARGS := --base 80000000 --pagesize 2048 --kernel_offset 00008000 --ramdisk_offset 04000000 --tags_offset 0e000000   --cmdline 'bootopt=64S3,32S1,32S1' --board B125 
 
@@ -18,4 +18,8 @@ TW_NO_EXFAT_FUSE := true
 TW_THEME := portrait_mdpi
 #RECOVERY_SDCARD_ON_DATA := true
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/gadget/lun%d/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file"
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TW_MAX_BRIGHTNESS := 255

f1b36d6ee5d2b237ea2b442457c703e767f6df6a
docs007 <34295814+docs007@users.noreply.github.com>
2018-11-12 11:12:28 +0600

    Delete cm.mk

--- cm.mk
+++
@@ -1,9 +0,0 @@
-# Inherit device configuration
-$(call inherit-product, device/huawei/Cairo_U00/device_Cairo_U00.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Cairo_U00
-PRODUCT_NAME := cm_Cairo_U00
-PRODUCT_BRAND := Huawei
-PRODUCT_MODEL := Cairo_U00
-PRODUCT_MANUFACTURER := Huawei
